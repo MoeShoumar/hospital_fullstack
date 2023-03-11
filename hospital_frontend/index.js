f01d9c0b32685f9b282c6118ab4b150ff95310ac
MoeShoumar <mks38@mail.aub.edu>
2023-03-11 16:19:29 +0200

    finished linking of 1st functioin in index page

--- hospital_frontend/index.js
+++ hospital_frontend/index.js
@@ -10,12 +10,16 @@ hospital_pages.getAPI = async (api_url) =>{
     }
 }
 
-hospital_pages.postAPI = async (api_url,api_data) =>{
+hospital_pages.postAPI = async (api_url,api_data, api_token = null) =>{
     try{
         return await axios.post(
             api_url,
             api_data,
-            
+            {
+                headers:{
+                    "Authorization" : "token" +api_token
+                }
+            }
             )
     }catch(error){
         console.log(error)
@@ -30,23 +34,29 @@ hospital_pages.loadFor = (page) => {
 hospital_pages.load_index = async ()=>{
     const form = document.getElementById("login-form");
     form.addEventListener("submit", async (event) => {
-        event.preventDefault()})
-    const email= document.getElementById("email").value;
-    const password= document.getElementById("password").value;
-    const data = { email: email, password: password };
-    const signin_url = hospital_pages.base_url + "signin.php"
-    const response=  hospital_pages.postAPI(signin_url,data)
-    
-    console.log(response.data); 
-    if (response.data.status !== null) {
+        event.preventDefault();
+        const email= document.getElementById("email").value;
+        const password= document.getElementById("password").value;
+        const data = { email: email, password: password };
+        const signin_url = hospital_pages.base_url + "signin.php";
+        const response = await hospital_pages.postAPI(signin_url,data);
+        console.log(response.data); 
+    if (response.data.status !== 'null'  && response.data.user_type === "3") {
+        localStorage.setItem('jwt', response.data.token);
         window.location.href = "adminpanel.html";}
+        else if (response.data.status !== "null" && response.data.user_type === "2") {
+            localStorage.setItem('jwt', response.data.token);
+            window.location.href = "employee.html";}
+        else if (response.data.status !== "null" && response.data.user_type === "1") {
+                localStorage.setItem('jwt', response.data.token);
+                window.location.href = "pateint.html";}
         else {
             const error_message = document.createElement("div");
             error_message.textContent = "Incorrect email or password.";
             form.appendChild(error_message);
           }
-
-    alert("heyyy")
+    
+    });
 }
 
 
@@ -66,3 +76,7 @@ hospital_pages.load_pateint = async ()=>{
 hospital_pages.load_employee = ()=>{
     alert(' hello employee')
 }
+
+// if (response.data.status !== null) { - This line of code is checking if response.data.status is not null. However, based on the API response you provided earlier, it seems like the status property is a string. So, you should check if response.data.status !== "null" instead.
+
+// window.location.href = "adminpanel.html"; - This line of code is redirecting the user to the adminpanel.html page regardless of whether they are an admin or not. You should first check the user's role (e.g. by decoding the JWT token) and then redirect them to the appropriate page.
