114ca2af834bbd3e1f4bd6cdc43885df5450feb9
MoeShoumar <mks38@mail.aub.edu>
2023-03-12 14:43:24 +0200

    fixed js issues in teh admin panel

--- hospital_frontend/index.js
+++ hospital_frontend/index.js
@@ -17,7 +17,7 @@ hospital_pages.postAPI = async (api_url,api_data, api_token = null) =>{
             api_data,
             {
                 headers:{
-                    "Authorization" : "token" +api_token
+                    "Authorization" : api_token
                 }
             }
             )
@@ -30,6 +30,8 @@ hospital_pages.loadFor = (page) => {
     eval ("hospital_pages.load_" + page+"();")
     
 }
+
+// sign-in
 hospital_pages.load_index = async ()=>{
     const form = document.getElementById("login-form");
     form.addEventListener("submit", async (event) => {
@@ -75,7 +77,7 @@ hospital_pages.load_admin = async ()=>{
       });   
      
     // get pateints
-    const form = document.getElementById("Assign_Patients_form");
+    
     const patients_url = hospital_pages.base_url + "get_patients.php";
     const response = await hospital_pages.getAPI(patients_url);
     const patients = response.data;
@@ -86,17 +88,36 @@ hospital_pages.load_admin = async ()=>{
       option.text = patient.name;
       patient_select.appendChild(option);
     });   
-   
+    //  get hospitals
+    const hospitals_url = hospital_pages.base_url + "get_hospital.php";
+    const response_hospital = await hospital_pages.getAPI(hospitals_url);
+    const hospitals = response_hospital.data;
+    const hospital_select = document.getElementById("hospital");
+    const hospital2_select = document.getElementById("hospital_2");
+    hospitals.forEach((hospital) => {
+        const option = document.createElement("option");
+        option.value = hospital.id;
+        option.text = hospital.name;
+        hospital_select.appendChild(option);
+        const option2 = document.createElement("option");
+        option2.value = hospital.id;
+        option2.text = hospital.name;
+        hospital2_select.appendChild(option2);
+      });   
+
     // assign pateints
+    const form = document.getElementById("Assign_Patients_form");
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
         const hospital= document.getElementById("hospital").value;
         console.log(hospital);
         const patient= document.getElementById("patient").value;
         console.log(patient);
+        const token_admin= localStorage.getItem('jwt');
+        console.log(token_admin);
         const data = { "hospital_id": hospital, "user_id": patient };
         const assign_patient_url = hospital_pages.base_url + "assign_patient.php";
-        const response = await hospital_pages.postAPI(assign_patient_url,data);
+        const response = await hospital_pages.postAPI(assign_patient_url,data,token_admin);
         console.log( response.data);
         console.log( response.status);
         if (response.status == 200) {
@@ -124,47 +145,53 @@ hospital_pages.load_admin = async ()=>{
             error_message.setAttribute("id", "error-message");
             error_message.textContent = response.data;
             form.appendChild(error_message);
-        } 
+        } })
 
         // assign employee
         const form_employees = document.getElementById("Assign_employees_form");
         form_employees.addEventListener("submit", async (event) => {
             event.preventDefault();
-            const hospital= document.getElementById("hospital").value;
+            const hospital= document.getElementById("hospital_2").value;
             console.log(hospital);
-            const patient= document.getElementById("employee").value;
+            const employee= document.getElementById("employee").value;
             console.log(employee);
             const data = { "hospital_id": hospital, "user_id": employee };
             const assign_employee_url = hospital_pages.base_url + "assign_employee.php";
-            const response = await hospital_pages.postAPI(assign_patient_url,data);
-            console.log( response.data);
-            console.log( response.status);
-            if (response.status == 200) {
-                const error_message = document.getElementById("error-message");
-                if (error_message) {
-                    error_message.remove();
+            const response2 = await hospital_pages.postAPI(assign_employee_url,data);
+            console.log( response2.data);
+            console.log( response2.status);
+            if (response2.status == 200) {
+                const error_message2 = document.getElementById("error-message");
+                if (error_message2) {
+                    error_message2.remove();
                 }
-                let success_message = document.getElementById("success-message");
-                if (success_message && response.data != "Patient assigned successfully") {
-                    success_message.remove();
-                    success_message = null;
+                let success_message2 = document.getElementById("success-message");
+                if (success_message2 && response2.data != "employee assigned successfully") {
+                    success_message2.remove();
+                    success_message2 = null;
                 }
-                if (!success_message) {
-                    success_message = document.createElement("div");
-                    success_message.setAttribute("id", "success-message");
-                    form.appendChild(success_message);
+                if (!success_message2) {
+                    success_message2 = document.createElement("p");
+                    success_message2.setAttribute("id", "success-message");
+                    form_employees.appendChild(success_message2);
                 }
-                success_message.textContent = response.data;
+                success_message2.textContent = response2.data;
             } else {
-                const success_message = document.getElementById("success-message");
-                if (success_message) {
-                    success_message.remove();
+                const success_message2 = document.getElementById("success-message");
+                if (success_message2) {
+                    success_message2.remove();
                 }
-                const error_message = document.createElement("div");
-                error_message.setAttribute("id", "error-message");
-                error_message.textContent = response.data;
-                form.appendChild(error_message);
+                const error_message2 = document.createElement("p");
+                error_message2.setAttribute("id", "error-message");
+                error_message2.textContent = response2.data;
+                form_employees.appendChild(error_message2);
             } 
+        // statistics
+        // female employee
+        const females= document.getElementById('female-employees')
+        const males= document.getElementById('Male-employees')
+        const AUBMC=document.getElementById('hospital-1-patients')
+        const MAKASED=document.getElementById('hospital-2-patients')
         
 
 
