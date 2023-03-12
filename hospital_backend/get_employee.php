af1616c169fffda7fb5d477641a7e9d94da580d6
MoeShoumar <mks38@mail.aub.edu>
2023-03-12 08:22:49 +0200

    get_employee api

---
+++ hospital_backend/get_employee.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+include('connection.php');
+
+$query = $mysqli->prepare("SELECT * FROM users where user_type='2'");
+$query->execute();
+$result = $query->get_result();
+$data = array();
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+echo json_encode($data);
