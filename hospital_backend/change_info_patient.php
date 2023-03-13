a41bd41d76c0e9d42fb451052ded11a4dd62267e
MoeShoumar <mks38@mail.aub.edu>
2023-03-13 08:03:13 +0200

    fixed some typos

--- hospital_backend/change_info_patient.php
+++ hospital_backend/change_info_patient.php
@@ -4,6 +4,8 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 include('connection.php');
 
+
+
 $user_id = $_GET['user_id'];
 $query = $mysqli->prepare("SELECT * FROM employees_info where user_id =? ");
 $query->bind_param('i', $user_id);
